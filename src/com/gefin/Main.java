97590e1bfe5954b1244cfbd0eafedfcb3e8b2548
gef10 <gefinsamuel@gmail.com>
2021-06-23 10:09:30 +0530

    my first commit of project

---
+++ src/com/gefin/Main.java
@@ -0,0 +1,11 @@
+package com.gefin;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   String a = sc.next();
+	   int b = sc.nextInt();
+	   System.out.println(a);
+	   System.out.println(b);	
+	}
+}
